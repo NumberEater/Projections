7295d2a6a2c3c794168b618a6882109a5942d777
NumberEater <reddickgrant@gmail.com>
2022-08-30 06:42:31 -0400

    Organized some things

--- Prototype-1/Program.py
+++ Prototype-1/Program.py
@@ -1,7 +1,9 @@
 import pygame as pg
 import numpy as np
 from math import sin, cos
-from Rotation import *
+from Box import *
+import random
+from os import system
 
 pg.init()
 screen = pg.display.set_mode((800, 800))
@@ -11,18 +13,7 @@ clock = pg.time.Clock()
 def center_origin(surf, p):
     return (p[0] + surf.get_width() / 2, p[1] + surf.get_height() / 2)
 
-points = np.array([
-    [-0.5, 0.5, -0.5],
-    [0.5, 0.5, -0.5],
-    [0.5, -0.5, -0.5],
-    [-0.5, -0.5, -0.5],
-    [-0.5, 0.5, 0.5],
-    [0.5, 0.5, 0.5],
-    [0.5, -0.5, 0.5],
-    [-0.5, -0.5, 0.5]
-])
-
-distance = 2
+distance = 3
 def project(rotated_z):
     z = 400 / (distance - rotated_z)
     projection_matrix = np.array([
@@ -35,50 +26,46 @@ def project(rotated_z):
 running = True
 rotation_angle = 0.0
 projected = []
-edges = [
-[0, 1], [1, 2], [2, 3], [3, 0],
-[4, 5], [5, 6], [6, 7], [7, 4],
-[0, 4], [1, 5], [2, 6], [3, 7]
-]
 
 scaleX = 400
 scaleY = 400
 
-rotationX = RotateX()
-rotationY = RotateY()
-rotationZ = RotateZ()
-rotationX.generate_matrix(0)
-rotationZ.generate_matrix(0)
-rotationY.generate_matrix(0)
+box = Box()
+
+doRotation = True
 
 while running:
     clock.tick(60)
-    pg.draw.rect(screen, (0, 0, 0), pg.Rect(0, 0, 800, 800))
+    screen.fill((0, 0, 0))
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
         elif event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 5:
-                distance += 0.5
+                distance += 0.2
             elif event.button == 4:
-                if distance >= 0.5:
-                    distance -= 0.5
-        elif event.type == pg.MOUSEMOTION:
-            mouse_position = pg.mouse.get_pos()
-            rotationY.generate_matrix(-mouse_position[0] / 200)
-            rotationX.generate_matrix(mouse_position[1] / 200)
-    for point in points:
-        rotation = np.matmul(point, rotationX.matrix)
-        rotation = np.matmul(rotation, rotationZ.matrix)
-        rotation = np.matmul(rotation, rotationY.matrix)
-        
+                if distance >= 0.2:
+                    distance -= 0.2
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                doRotation = not doRotation
+
+    if doRotation:
+        box.PushAngleX(rotation_angle)
+        rotation_angle += 0.01
+
+    for point in box.points:
+        rotation = np.matmul(point, box.GetRotationMatrix())
         projection = np.matmul(rotation, project(rotation[2]))
+        #print(projection[0], projection[1])
         projected.append((projection[0], projection[1]))
-    for edge in edges:
-        pg.draw.line(screen, (255, 255, 255), center_origin(screen, projected[edge[0]]), center_origin(screen, projected[edge[1]]), 3)
+        pg.draw.circle(screen, (255, 255, 255), center_origin(screen, (projection[0], projection[1])), 15 - distance)
+    for edge in box.edges:
+        pg.draw.line(screen, (255, 255, 255), center_origin(screen, projected[edge[0]]), center_origin(screen, projected[edge[1]]), 2)
     projected = []
 
     pg.display.update()
-    rotation_angle += 0.01
+    
+    
 
 pg.quit()
