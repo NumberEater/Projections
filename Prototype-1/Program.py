eb3f5825841e2cdc2753393154ce7ff8bd74733b
NumberEater <reddickgrant@gmail.com>
2022-09-01 09:43:13 -0400

    Organized more

--- Prototype-1/Program.py
+++ Prototype-1/Program.py
@@ -1,71 +1,34 @@
 import pygame as pg
-import numpy as np
-from math import sin, cos
-from Box import *
-import random
+from Shapes.Box import *
 from os import system
+from Renderer import *
+from Shapes.Pyramid import *
 
-pg.init()
-screen = pg.display.set_mode((800, 800))
-pg.display.set_caption("Projection")
-clock = pg.time.Clock()
+class ExampleProgram:
+    def RunExample(self):
+        # Pygame stuff
+        pg.init()
+        screen = pg.display.set_mode((640, 480), vsync=1)
+        pg.display.set_caption("Projection")
+        clock = pg.time.Clock()
+        running = True
+        box1 = Box(Vector3(1.0, 1.0, 1.0))
+        pyramid = Pyramid(Vector3(1, 1, 1))
 
-def center_origin(surf, p):
-    return (p[0] + surf.get_width() / 2, p[1] + surf.get_height() / 2)
+        renderer = Renderer(screen, [pyramid], distance=5)
 
-distance = 3
-def project(rotated_z):
-    z = 400 / (distance - rotated_z)
-    projection_matrix = np.array([
-        [z, 0, 0],
-        [0, z, 0],
-        [0, 0, 0]
-    ])
-    return projection_matrix
+        while running:
+            screen.fill((70, 80, 100))  # Clears the screen
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+                elif event.type == pg.MOUSEMOTION:
+                    pyramid.PushAngleY(-(pg.mouse.get_pos()[0] / (screen.get_width() / 360)))  # Move the pyramid with the mouse
+                    
+            renderer.RenderFrame()  # Tell renderer to render stuff
+            pg.display.update()  # Update the screen
 
-running = True
-rotation_angle = 0.0
-projected = []
+        pg.quit()
 
-scaleX = 400
-scaleY = 400
-
-box = Box()
-
-doRotation = True
-
-while running:
-    clock.tick(60)
-    screen.fill((0, 0, 0))
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-        elif event.type == pg.MOUSEBUTTONDOWN:
-            if event.button == 5:
-                distance += 0.2
-            elif event.button == 4:
-                if distance >= 0.2:
-                    distance -= 0.2
-        elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_SPACE:
-                doRotation = not doRotation
-
-    if doRotation:
-        box.PushAngleX(rotation_angle)
-        rotation_angle += 0.01
-
-    for point in box.points:
-        rotation = np.matmul(point, box.GetRotationMatrix())
-        projection = np.matmul(rotation, project(rotation[2]))
-        #print(projection[0], projection[1])
-        projected.append((projection[0], projection[1]))
-        pg.draw.circle(screen, (255, 255, 255), center_origin(screen, (projection[0], projection[1])), 15 - distance)
-    for edge in box.edges:
-        pg.draw.line(screen, (255, 255, 255), center_origin(screen, projected[edge[0]]), center_origin(screen, projected[edge[1]]), 2)
-    projected = []
-
-    pg.display.update()
-    
-    
-
-pg.quit()
+program = ExampleProgram()
+program.RunExample()
