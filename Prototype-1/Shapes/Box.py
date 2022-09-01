7a828bf7d328eaf0fc984489d4fa41a6e3c84381
NumberEater <reddickgrant@gmail.com>
2022-09-01 09:44:56 -0400

    Refactor

---
+++ Prototype-1/Shapes/Box.py
@@ -0,0 +1,8 @@
+import numpy as np
+from math import sin, cos, atan2
+from Vector3 import *
+from RenderableObject import *
+
+# Just a different name for the default renderable object
+class Box(RenderableObject):
+    pass
